9d250e719c7a55f7ac482060bce018b9f89513c9
Jonas Laacke <jonas.laacke@iteratec.de>
2015-05-07 14:35:10 +0200

    added LoadDataChangeMSSQL with same behaviour as InsertDataChangeMSSQL

---
+++ src/java/liquibase/ext/mssql/change/LoadDataChangeMSSQL.java
@@ -0,0 +1,38 @@
+package liquibase.ext.mssql.change;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import liquibase.change.ChangeMetaData;
+import liquibase.change.DatabaseChange;
+import liquibase.database.Database;
+import liquibase.ext.mssql.statement.InsertStatementMSSQL;
+import liquibase.statement.SqlStatement;
+import liquibase.statement.core.InsertStatement;
+
+@DatabaseChange(name = "loadData", description = "Load Data", priority = ChangeMetaData.PRIORITY_DEFAULT, appliesTo = "table")
+public class LoadDataChangeMSSQL extends liquibase.change.core.LoadDataChange {
+    private Boolean identityInsertEnabled;
+
+    public Boolean getIdentityInsertEnabled() {
+	return identityInsertEnabled;
+    }
+
+    public void setIdentityInsertEnabled(Boolean identityInsertEnabled) {
+	this.identityInsertEnabled = identityInsertEnabled;
+    }
+
+    @Override
+    public SqlStatement[] generateStatements(Database database) {
+	SqlStatement[] statements = super.generateStatements(database);
+	List<SqlStatement> wrappedStatements = new ArrayList<SqlStatement>(statements.length);
+	for (SqlStatement statement : statements) {
+	    if (statement instanceof InsertStatement) {
+		wrappedStatements.add(new InsertStatementMSSQL((InsertStatement) statement, identityInsertEnabled));
+	    } else {
+		wrappedStatements.add(statement);
+	    }
+	}
+	return wrappedStatements.toArray(new SqlStatement[0]);
+    }
+}
