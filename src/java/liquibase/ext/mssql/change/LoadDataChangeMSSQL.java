3554cf68751d09d41ff5e8d01b50dec720fa89d7
Jonas Laacke <jonas.laacke@iteratec.de>
2015-10-08 16:03:01 +0200

    increased liquibase version to 3.4.1 (includes introduction of InsertSetStatement)

--- src/java/liquibase/ext/mssql/change/LoadDataChangeMSSQL.java
+++ src/java/liquibase/ext/mssql/change/LoadDataChangeMSSQL.java
@@ -6,8 +6,10 @@ import java.util.List;
 import liquibase.change.ChangeMetaData;
 import liquibase.change.DatabaseChange;
 import liquibase.database.Database;
+import liquibase.ext.mssql.statement.InsertSetStatementMSSQL;
 import liquibase.ext.mssql.statement.InsertStatementMSSQL;
 import liquibase.statement.SqlStatement;
+import liquibase.statement.core.InsertSetStatement;
 import liquibase.statement.core.InsertStatement;
 
 @DatabaseChange(name = "loadData", description = "Load Data", priority = ChangeMetaData.PRIORITY_DATABASE, appliesTo = "table")
@@ -29,6 +31,8 @@ public class LoadDataChangeMSSQL extends liquibase.change.core.LoadDataChange {
 	for (SqlStatement statement : statements) {
 	    if (statement instanceof InsertStatement) {
 		wrappedStatements.add(new InsertStatementMSSQL((InsertStatement) statement, identityInsertEnabled));
+	    } else if(statement instanceof InsertSetStatement) {
+	        wrappedStatements.add(new InsertSetStatementMSSQL((InsertSetStatement) statement, identityInsertEnabled));
 	    } else {
 		wrappedStatements.add(statement);
 	    }
---
+++ src/java/liquibase/ext/mssql/sqlgenerator/InsertSetGenerator.java
@@ -0,0 +1,87 @@
+package liquibase.ext.mssql.sqlgenerator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import liquibase.database.Database;
+import liquibase.database.core.MSSQLDatabase;
+import liquibase.exception.ValidationErrors;
+import liquibase.ext.mssql.statement.InsertSetStatementMSSQL;
+import liquibase.sql.Sql;
+import liquibase.sql.UnparsedSql;
+import liquibase.sqlgenerator.SqlGeneratorChain;
+import liquibase.statement.core.InsertSetStatement;
+import liquibase.statement.core.InsertStatement;
+
+public class InsertSetGenerator extends liquibase.sqlgenerator.core.InsertSetGenerator {
+    public static final String IF_TABLE_HAS_IDENTITY_STATEMENT = "IF ((select objectproperty(\n"
+                    + "            object_id(N'${schemaName}.${tableName}'),\n"
+                    + "           'TableHasIdentity')) = 1)\n" + "\t${then}\n";
+
+    @Override
+    public int getPriority() {
+        return 15;
+    }
+
+    public boolean supports(InsertStatement statement, Database database) {
+        return database instanceof MSSQLDatabase;
+    }
+
+    public ValidationErrors validate(InsertStatement statement, Database database,
+                    SqlGeneratorChain sqlGeneratorChain) {
+        return sqlGeneratorChain.validate(statement, database);
+    }
+
+    @Override
+    public Sql[] generateSql(InsertSetStatement statement, Database database, SqlGeneratorChain sqlGeneratorChain) {
+        Boolean identityInsertEnabled = false;
+        if (statement instanceof InsertSetStatementMSSQL) {
+            identityInsertEnabled = ((InsertSetStatementMSSQL) statement).getIdentityInsertEnabled();
+        }
+        if (identityInsertEnabled == null || !identityInsertEnabled) {
+            return super.generateSql(statement, database, sqlGeneratorChain);
+        }
+        String tableName = database.escapeTableName(statement.getCatalogName(), statement.getSchemaName(),
+                        statement.getTableName());
+        String enableIdentityInsert = "SET IDENTITY_INSERT " + tableName + " ON";
+        String disableIdentityInsert = "SET IDENTITY_INSERT " + tableName + " OFF";
+        String safelyEnableIdentityInsert = ifTableHasIdentityColumn(enableIdentityInsert, statement,
+                        database.getDefaultSchemaName());
+        String safelyDisableIdentityInsert = ifTableHasIdentityColumn(disableIdentityInsert, statement,
+                        database.getDefaultSchemaName());
+
+        List<Sql> sql = new ArrayList<Sql>(Arrays.asList(sqlGeneratorChain.generateSql(statement, database)));
+        sql.add(0, new UnparsedSql(safelyEnableIdentityInsert));
+        sql.add(new UnparsedSql(safelyDisableIdentityInsert));
+        return sql.toArray(new Sql[sql.size()]);
+    }
+
+    private String ifTableHasIdentityColumn(String then, InsertSetStatement statement, String defaultSchemaName) {
+        String tableName = statement.getTableName();
+        String schemaName = statement.getSchemaName();
+        if (schemaName == null) {
+            if (defaultSchemaName != null && !defaultSchemaName.isEmpty()) {
+                schemaName = defaultSchemaName;
+            } else {
+                schemaName = "dbo";
+            }
+        }
+
+        Map<String, String> tokens = new HashMap<String, String>();
+        tokens.put("${tableName}", tableName);
+        tokens.put("${schemaName}", schemaName);
+        tokens.put("${then}", then);
+        return performTokenReplacement(IF_TABLE_HAS_IDENTITY_STATEMENT, tokens);
+    }
+
+    private String performTokenReplacement(String input, Map<String, String> tokens) {
+        String result = input;
+        for (Map.Entry<String, String> entry : tokens.entrySet()) {
+            result = result.replace(entry.getKey(), entry.getValue());
+        }
+        return result;
+    }
+}
